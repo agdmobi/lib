bc21982cedc15c2c59b188fa132c465a403def88
Agooday <vinance.dev@gmail.com>
2019-04-16 03:03:14 +0700

    pick file

--- app/src/main/java/com/agooday/library/MainActivity.java
+++ app/src/main/java/com/agooday/library/MainActivity.java
@@ -1,35 +1,123 @@
 package com.agooday.library;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.agooday.filepicker.Constant;
+import com.agooday.filepicker.activity.AudioPickActivity;
+import com.agooday.filepicker.activity.ImagePickActivity;
+import com.agooday.filepicker.activity.NormalFilePickActivity;
+import com.agooday.filepicker.activity.VideoPickActivity;
+import com.agooday.filepicker.filter.entity.AudioFile;
+import com.agooday.filepicker.filter.entity.ImageFile;
+import com.agooday.filepicker.filter.entity.NormalFile;
+import com.agooday.filepicker.filter.entity.VideoFile;
+
+import java.util.ArrayList;
 
 
-import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mTvResult;
+    public static final String IS_NEED_FOLDER_LIST = "isNeedFolderList";
+    public static final String IS_NEED_CAMERA = "IsNeedCamera";
+    public static final String IS_NEED_IMAGE_PAGER = "IsNeedImagePager";
+    public static final String IS_TAKEN_AUTO_SELECTED = "IsTakenAutoSelected";
+    public static final String IS_NEED_RECORDER = "IsNeedRecorder";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new RangeDatePickerDialog(this).show();
-        //SlyCalendarView d = findViewById(R.id.calendar);
-        //Log.d("tien.hien","null = "+(d==null));
+
+        mTvResult = (TextView) findViewById(R.id.tv_result);
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_pick_image:
+                Intent intent1 = new Intent(this, ImagePickActivity.class);
+                intent1.putExtra(IS_NEED_CAMERA, true);
+                intent1.putExtra(Constant.MAX_NUMBER, 9);
+                intent1.putExtra(IS_NEED_FOLDER_LIST, true);
+                startActivityForResult(intent1, Constant.REQUEST_CODE_PICK_IMAGE);
+                break;
+            case R.id.btn_pick_video:
+                Intent intent2 = new Intent(this, VideoPickActivity.class);
+                intent2.putExtra(IS_NEED_CAMERA, true);
+                intent2.putExtra(Constant.MAX_NUMBER, 9);
+                intent2.putExtra(IS_NEED_FOLDER_LIST, true);
+                startActivityForResult(intent2, Constant.REQUEST_CODE_PICK_VIDEO);
+                break;
+            case R.id.btn_pick_audio:
+                Intent intent3 = new Intent(this, AudioPickActivity.class);
+                intent3.putExtra(IS_NEED_RECORDER, true);
+                intent3.putExtra(Constant.MAX_NUMBER, 9);
+                intent3.putExtra(IS_NEED_FOLDER_LIST, true);
+                startActivityForResult(intent3, Constant.REQUEST_CODE_PICK_AUDIO);
+                break;
+            case R.id.btn_pick_file:
+                Intent intent4 = new Intent(this, NormalFilePickActivity.class);
+                intent4.putExtra(Constant.MAX_NUMBER, 9);
+                intent4.putExtra(IS_NEED_FOLDER_LIST, true);
+                intent4.putExtra(NormalFilePickActivity.SUFFIX,
+                        new String[]{"xlsx", "xls", "doc", "dOcX", "ppt", ".pptx", "pdf"});
+                startActivityForResult(intent4, Constant.REQUEST_CODE_PICK_FILE);
+                break;
+        }
+    }
 
-    /*private fun pickDate() {
-       calendarView.setCallback(object : Callback, com.agooday.daterangepicker.Callback {
-            override fun onDataSelected(firstDate: Calendar?, secondDate: Calendar?, hours: Int, minutes: Int) =
-                if (firstDate != null && secondDate != null) {
-                    listener.okConfirm(firstDate, secondDate, hours, minutes)
-                    dismiss()
-                } else onCancelled()
-
-            override fun onCancelled() {
-                listener.onCancel()
-                dismiss()
-            }
-
-        })
-    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case Constant.REQUEST_CODE_PICK_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<ImageFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_IMAGE);
+                    StringBuilder builder = new StringBuilder();
+                    for (ImageFile file : list) {
+                        String path = file.getPath();
+                        builder.append(path + "\n");
+                    }
+                    mTvResult.setText(builder.toString());
+                }
+                break;
+            case Constant.REQUEST_CODE_PICK_VIDEO:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<VideoFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_VIDEO);
+                    StringBuilder builder = new StringBuilder();
+                    for (VideoFile file : list) {
+                        String path = file.getPath();
+                        builder.append(path + "\n");
+                    }
+                    mTvResult.setText(builder.toString());
+                }
+                break;
+            case Constant.REQUEST_CODE_PICK_AUDIO:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<AudioFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_AUDIO);
+                    StringBuilder builder = new StringBuilder();
+                    for (AudioFile file : list) {
+                        String path = file.getPath();
+                        builder.append(path + "\n");
+                    }
+                    mTvResult.setText(builder.toString());
+                }
+                break;
+            case Constant.REQUEST_CODE_PICK_FILE:
+                if (resultCode == RESULT_OK) {
+                    ArrayList<NormalFile> list = data.getParcelableArrayListExtra(Constant.RESULT_PICK_FILE);
+                    StringBuilder builder = new StringBuilder();
+                    for (NormalFile file : list) {
+                        String path = file.getPath();
+                        builder.append(path + "\n");
+                    }
+                    mTvResult.setText(builder.toString());
+                }
+                break;
+        }
+    }
 }
